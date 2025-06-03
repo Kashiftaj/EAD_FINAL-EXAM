a4da3a52a07548846bec8c980ec07618384086b3
Kashiftaj <139766663+Kashiftaj@users.noreply.github.com>
2025-06-03 10:58:29 +0500

    project setup only lights added

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ function App() {
   return (
     <div>
       <h1 className='bg-black text-white text-3xl'>hello finals</h1>
+      
     </div>
   )
 }
+
 
 export default App
---
+++ src/components/TrafficLightSimulator.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react'
+import {useState} from React
+
+function TrafficLightSimulator() {
+    const [lights,setLights]=useState({
+      red : true,
+      yellow: false,
+      green:false
+    })
+  return (
+
+    <div>
+      
+    </div>
+  )
+}
+
+export default TrafficLightSimulator
